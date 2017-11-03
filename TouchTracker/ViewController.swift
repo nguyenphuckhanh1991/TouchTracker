e8a5955072cf11cbf5da212258b370ac6219c9f5
nguyen.phuc.khanh <nguyen.phuc.khanh@framgias-mac-mini.local>
2017-11-03 10:51:44 +0700

    Initial Commit

---
+++ TouchTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by nguyen.phuc.khanh on 11/3/17.
+//  Copyright © 2017 nguyen.phuc.khanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
